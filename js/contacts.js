713dd927f83adc17b6298e3366a24514ce10c67e
g1un <serbov@pochta.ru>
2016-02-02 11:45:22 +0500

    deposit-message is added

--- js/contacts.js
+++ js/contacts.js
@@ -1,6 +1,43 @@
 /*contacts.js*/
-if(document.querySelector('.jq-selectbox-main')){
-  $('select').styler();
-}
+$(document).ready(function() {
+  if (document.querySelector('.jq-selectbox-main')) {
+    $('select').styler();
+  }
+
+  /*deposit message*/
+  if ($('.deposit-message__wrapper').length) {
+
+    var deposit = $('.deposit-message__wrapper');
+    var depositMsg = $('.deposit-message');
+    var depositBtn = $('.deposit-message__btn');
+
+    function depositCss() {
+      if (window.innerHeight >= depositMsg.outerHeight()) {
+        var depositMargin = (window.innerHeight - depositMsg.outerHeight() - $('.b-static-stripe').outerHeight()) / 2;
+        depositMsg.css('margin-top', depositMargin);
+      }
+    }
+
+    depositCss();
+
+    $(window).on('resize', function () {
+      depositCss();
+    });
+
+    depositBtn.on('click', function () {
+      deposit.css('display', 'none');
+    });
+
+    deposit.mousedown(function (e) {
+      var clicked = $(e.target);
+      if (clicked.is('.deposit-message') || clicked.parents().is('.deposit-message')) {
+        return;
+      } else {
+        deposit.hide();
+      }
+    });
+  }
+});
+/*end of deposit message*/
 /*end of contacts.js*/
 
