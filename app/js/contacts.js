4e05d3a8f1befc194ae3d90f36dbc9d0787f88a6
I.Smal <serbov@pochta.ru>
2016-03-30 17:24:38 +0500

    Shipstores_magazin_business_public, Shipstores_magazin_light_public, Shipstores_magazin_pro_public

--- app/js/contacts.js
+++ app/js/contacts.js
@@ -752,6 +752,42 @@ $(document).ready(function() {
 		}
 	}
 	/*end of b-ad-unit-block__gallery*/
+
+	/*map everywhere*/
+	if($('.map').length){
+
+		function init () {
+			var myMap = new ymaps.Map("map", {
+						center: [59.937314,30.315472],
+						zoom: 11
+					}),
+
+			// Создаем метку с помощью вспомогательного класса.
+					myPlacemark1 = new ymaps.Placemark([59.937314,30.315472], {
+						hintContent: '',
+						//balloonContent: ''
+					}, {
+						// Опции.
+						// Необходимо указать данный тип макета.
+						iconLayout: 'default#image',
+						// Своё изображение иконки метки.
+						iconImageHref: 'images/placemarker.png',
+						// Размеры метки.
+						iconImageSize: [24, 32],
+						// Смещение левого верхнего угла иконки относительно
+						// её "ножки" (точки привязки).
+						iconImageOffset: [-3, -42]
+					});
+
+			//myMap.controls.add('smallZoomControl');
+			// Добавляем все метки на карту.
+			myMap.geoObjects
+					.add(myPlacemark1);
+		}
+
+		ymaps.ready(init);
+	}
+	/*end of map everywhere*/
 });
 /*end of contacts.js*/
 
